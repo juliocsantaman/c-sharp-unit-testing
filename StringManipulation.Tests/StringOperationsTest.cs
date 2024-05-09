c462f879fd2f6701b5361d222bc28be5f2db57ba
Julio Cesar Santaman Cruz <juliocsantaman@gmail.com>
2024-05-09 00:17:45 -0500

    Test for ConcatenateStrings method.

---
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation.Tests
+{
+    public class StringOperationsTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var strOperation = new StringOperations();
+            var result = strOperation.ConcatenateStrings("Hello", "World");
+            Assert.Equal("Hello World", result);
+        }
+    }
+}
