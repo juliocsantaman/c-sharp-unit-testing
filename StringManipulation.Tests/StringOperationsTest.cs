f0006476a5da92ad7177ee5702ea13c961bb1ef8
Julio Cesar Santaman Cruz <juliocsantaman@gmail.com>
2024-05-10 12:51:59 -0500

    Test for IsPalindrome_True and IsPalindrome_False

--- StringManipulation.Tests/StringOperationsTest.cs
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -1,19 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StringManipulation.Tests
+﻿namespace StringManipulation.Tests
 {
     public class StringOperationsTest
     {
         [Fact]
         public void ConcatenateStrings()
         {
+            // Arrange.
             var strOperation = new StringOperations();
+
+            // Act.
             var result = strOperation.ConcatenateStrings("Hello", "World");
+
+            // Assert.
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
             Assert.Equal("Hello World", result);
         }
+
+        [Fact]
+        public void IsPalindrome_True()
+        {
+            // Arrange.
+            var strOperation = new StringOperations();
+
+            // Act.
+            var result = strOperation.IsPalindrome("Ana");
+
+            // Assert.
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindrome_False()
+        {
+            // Arrange.
+            var strOperation = new StringOperations();
+
+            // Act.
+            var result = strOperation.IsPalindrome("Test");
+
+            // Assert.
+            Assert.False(result);
+        }
+
     }
 }
